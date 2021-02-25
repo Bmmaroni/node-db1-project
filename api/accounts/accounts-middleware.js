d30f8600b46e5c782ebec56e753a3d22eff52d1c
Brandon Maroni <63432771+Bmmaroni@users.noreply.github.com>
2021-02-24 19:54:50 -0700

    finished checkAccountPayload

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -8,7 +8,23 @@ exports.checkAccountPayload = (req, res, next) => {
     res.json({
       message: "name and budget are required"
     })
-  } else if ()
+  } else if (typeof req.body.name !== 'string' ) {
+    res.json({
+      message: "name of account must be a string"
+    })
+  } else if (req.body.name.length < 3 || req.body.name.length > 100) {
+    res.json({
+      message: "name of account must be between 3 and 100"
+    })
+  } else if (typeof req.body.budget !== 'number') {
+    res.json({
+      message: "budget of account must be a number"
+    })
+  } else if (req.body.budget < 0 || req.body.budget > 1000000) {
+    res.json({
+      message: "budget of account is too large or too small"
+    })
+  }
 }
 
 exports.checkAccountNameUnique = async (req, res, next) => {

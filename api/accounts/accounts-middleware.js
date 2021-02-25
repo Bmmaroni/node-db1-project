5703022ee79b7af188d130a9d06927350b5cd750
Brandon Maroni <63432771+Bmmaroni@users.noreply.github.com>
2021-02-24 19:26:12 -0700

    created all functions for accounts-model and began work on middleware

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,5 +1,14 @@
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
+  if (!req.body) {
+    res.status(400).json({
+      message: ""
+    })
+  } else if (!req.body.name || !req.body.budget) {
+    res.json({
+      message: "name and budget are required"
+    })
+  } else if ()
 }
 
 exports.checkAccountNameUnique = async (req, res, next) => {
